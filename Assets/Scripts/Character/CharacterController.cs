adcba51ec3ac429dc201ff2dbad8c4c2f6756180
RSzyh <750332908@qq.com>
2018-09-16 15:25:29 +0800

    unit character controller

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+	public bool jumping;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		jumping = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!jumping && Input.GetMouseButtonDown(0)) {
+			jumping = true;
+			rb.velocity = new Vector2(rb.velocity.x, 10.0f);
+		}
+	}
+
+	public void StopJumping() {
+		jumping = false;
+	}
+}
