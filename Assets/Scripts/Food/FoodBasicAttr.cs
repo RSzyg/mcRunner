d9f8be6f8b3209dce441041003eb94916ac387f0
RSzyh <750332908@qq.com>
2018-09-18 19:51:42 +0800

    add attribute to mooncake

---
+++ Assets/Scripts/Food/FoodBasicAttr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBasicAttr : MonoBehaviour {
+	public float energy;
+
+	private void Awake()
+	{
+		energy = 6.0f;
+	}
+}
