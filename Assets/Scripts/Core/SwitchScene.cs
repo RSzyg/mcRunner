e8c3d0b75185e0792595d453c80224bfc87d418d
RSzyh <750332908@qq.com>
2018-09-19 17:40:05 +0800

    add load scene method

---
+++ Assets/Scripts/Core/SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+	public void LoadSceneById(int id) {
+		SceneManager.LoadScene(id, LoadSceneMode.Single);
+	}
+}
